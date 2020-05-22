be97d9b46eaff72603ea1b3d419b8e18cb323fa9
sim-my <simranpanthi101@gmail.com>
2020-05-22 11:45:35 +0545

    Connected form value to the model

--- home.py
+++ home.py
@@ -1,6 +1,32 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import pandas as pd
+import id3
 app = Flask(__name__)
+
 
 @app.route("/")
 def hello():
     return render_template('home.html')
+
+@app.route("/result", methods=['POST'])
+def result():
+    form_data=request.form
+    form_data=form_data.to_dict()
+    for keys in form_data: 
+        form_data[keys] = int(form_data[keys])
+    print(form_data)
+    final_result = str(id3.predict(form_data,id3.tree))
+    if (final_result == "1"):
+        final_result = "Below 50%"
+    elif (final_result == "2"):
+        final_result == "50-60%"
+    elif (final_result == "3"):
+        final_result == "60-70%"
+    elif (final_result == "4"):
+        final_result == "70-80%"
+    elif (final_result == "5"):
+        final_result == "80-90%"
+    elif (final_result == "6"):
+        final_result == "Above 90%"
+    print("The marks is " + final_result)
+    return render_template('result.html')
--- main.py
+++ main.py
@@ -3,6 +3,8 @@
 import pandas as pd
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn import metrics
 
 #dataset read
 dataset = pd.read_csv('static/performance.csv')
@@ -14,3 +16,19 @@ y = dataset.iloc[:,-1:].values
 #Since all the data collected are categorical data
 onehotencoder = OneHotEncoder(categories = "auto")
 X = onehotencoder.fit_transform(X).toarray()
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+#Using CART
+
+# Create Decision Tree classifer object
+clf = DecisionTreeClassifier()
+
+# Train Decision Tree Classifer
+clf = clf.fit(X_train,y_train)
+
+#Predict the response for test dataset
+y_pred = clf.predict(X_test)
+
+#calculate accuracy of the prediction
+print("Accuracy:",metrics.accuracy_score(y_test, y_pred))
